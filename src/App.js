6cf199fb2a1b6d67589db65446cc2aa54aaed411
JC Carrillo <jccarrillo@acm.org>
2021-08-30 17:42:24 -0400

    formatted

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          Coming Soon!
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                Coming Soon!
+            </header>
+        </div>
+    );
 }
 
 export default App;
