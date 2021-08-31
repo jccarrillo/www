1e9f6b81b6af0a11b0f01e6cff53cba0ca3116c6
JC Carrillo <jccarrillo@acm.org>
2021-08-31 10:38:52 -0400

    drawer and home

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import * as React from 'react';
-import { styled, alpha } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
+import {alpha, CSSObject, styled, Theme, useTheme} from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
+import MuiDrawer from '@material-ui/core/Drawer';
+import MuiAppBar, {AppBarProps as MuiAppBarProps} from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
 import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
 import MenuIcon from '@material-ui/icons/Menu';
 import SettingsIcon from '@material-ui/icons/Settings';
 import SearchIcon from '@material-ui/icons/Search';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import JcPortrait from './jc_portrait.jpeg';
+
+const drawerWidth = 240;
 
 const Search = styled('div')(({ theme }) => ({
     position: 'relative',
@@ -32,7 +42,7 @@ const Search = styled('div')(({ theme }) => ({
     },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled('div')(({theme}) => ({
     padding: theme.spacing(0, 2),
     height: '100%',
     position: 'absolute',
@@ -42,7 +52,7 @@ const SearchIconWrapper = styled('div')(({ theme }) => ({
     justifyContent: 'center',
 }));
 
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
+const StyledInputBase = styled(InputBase)(({theme}) => ({
     color: 'inherit',
     '& .MuiInputBase-input': {
         padding: theme.spacing(1, 1, 1, 0),
@@ -56,115 +66,117 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     },
 }));
 
+const openedMixin = (theme: Theme): CSSObject => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+        width: `calc(${theme.spacing(9)} + 1px)`,
+    },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+    open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({theme, open}) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+
+const Drawer = styled(MuiDrawer, {shouldForwardProp: (prop) => prop !== 'open'})(
+    ({theme, open}) => ({
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        ...(open && {
+            ...openedMixin(theme),
+            '& .MuiDrawer-paper': openedMixin(theme),
+        }),
+        ...(!open && {
+            ...closedMixin(theme),
+            '& .MuiDrawer-paper': closedMixin(theme),
+        }),
+    }),
+);
+
 export default function PrimarySearchAppBar() {
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
     const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
         React.useState<null | HTMLElement>(null);
 
-    const isMenuOpen = Boolean(anchorEl);
-    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
     const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorEl(event.currentTarget);
-    };
-
-    const handleMobileMenuClose = () => {
-        setMobileMoreAnchorEl(null);
-    };
-
-    const handleMenuClose = () => {
-        setAnchorEl(null);
-        handleMobileMenuClose();
     };
 
     const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
         setMobileMoreAnchorEl(event.currentTarget);
     };
 
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
     const menuId = 'primary-search-account-menu';
-    const renderMenu = (
-        <Menu
-            anchorEl={anchorEl}
-            anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-            }}
-            id={menuId}
-            keepMounted
-            transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-            }}
-            open={isMenuOpen}
-            onClose={handleMenuClose}
-        >
-            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-            <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-        </Menu>
-    );
 
     const mobileMenuId = 'primary-search-account-menu-mobile';
-    const renderMobileMenu = (
-        <Menu
-            anchorEl={mobileMoreAnchorEl}
-            anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-            }}
-            id={mobileMenuId}
-            keepMounted
-            transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-            }}
-            open={isMobileMenuOpen}
-            onClose={handleMobileMenuClose}
-        >
-            <MenuItem>
-                <IconButton
-                    size="large"
-                    aria-label="show 17 new notifications"
-                    color="inherit"
-                >
-                    <Badge badgeContent={17} color="error">
-                        <NotificationsIcon />
-                    </Badge>
-                </IconButton>
-                <p>Notifications</p>
-            </MenuItem>
-            <MenuItem>
-                <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-                    <Badge badgeContent={4} color="error">
-                        <SettingsIcon />
-                    </Badge>
-                </IconButton>
-                <p>Messages</p>
-            </MenuItem>
-            <MenuItem onClick={handleProfileMenuOpen}>
-                <IconButton
-                    size="large"
-                    aria-label="account of current user"
-                    aria-controls="primary-search-account-menu"
-                    aria-haspopup="true"
-                    color="inherit"
-                >
-                    <AccountCircle />
-                </IconButton>
-                <p>Profile</p>
-            </MenuItem>
-        </Menu>
-    );
 
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
+        <Box sx={{display: 'flex'}}>
+            <CssBaseline/>
+            <AppBar position="fixed" open={open}>
                 <Toolbar>
                     <IconButton
-                        size="large"
-                        edge="start"
                         color="inherit"
                         aria-label="open drawer"
-                        sx={{ mr: 2 }}
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        sx={{
+                            marginRight: '36px',
+                            ...(open && {display: 'none'}),
+                        }}
                     >
                         <MenuIcon />
                     </IconButton>
@@ -225,8 +237,97 @@ export default function PrimarySearchAppBar() {
                     </Box>
                 </Toolbar>
             </AppBar>
-            {renderMobileMenu}
-            {renderMenu}
+            <Drawer variant="permanent" open={open}>
+                <DrawerHeader>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'rtl' ? <ChevronRightIcon/> : <ChevronLeftIcon/>}
+                    </IconButton>
+                </DrawerHeader>
+                <Divider/>
+                <List>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <NotificationsIcon/> : <SettingsIcon/>}
+                            </ListItemIcon>
+                            <ListItemText primary={text}/>
+                        </ListItem>
+                    ))}
+                </List>
+                <Divider/>
+                <List>
+                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <NotificationsIcon/> : <SettingsIcon/>}
+                            </ListItemIcon>
+                            <ListItemText primary={text}/>
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+            <Box component="main" sx={{flexGrow: 1, p: 3}}>
+                <DrawerHeader/>
+                <Box component="main" sx={{flexGrow: 1, p: 3}}>
+                    <Box style={{float: 'right', height: '300px', margin: '0px 0px 20px 20px'}} height="10%">
+                        <img src={JcPortrait} className="App-logo" alt="logo" style={{height: '100%'}}/>
+                    </Box>
+                    <Typography paragraph>
+                        <b>Why is Software Engineering Important?</b>
+                    </Typography>
+                    <Typography paragraph>
+                        Because it provides a systematic approach to creating and maintaining that which has become the
+                        pilar of every industry - <b>software</b>.
+                    </Typography>
+                    <Typography paragraph>
+                        Software is a collection of programs and/or data used by a computer to perform tasks while
+                        software engineering is the use of principles to design, build and maintain it. A simple analogy
+                        to Software Engineering is "Cooking". A cook does not simply prepare a meal; A cook understands
+                        the effects on nutritional content of the food, kitchen safety as well as food safety and as a
+                        result of this knowledge and understanding, a cook follows a set of principles to ensure that
+                        the food is prepared correctly. The recipe, in the other hand, can be compared to "code", which
+                        is usually confused with "software."
+                    </Typography>
+                    <Typography paragraph>
+                        Blurring the definition of Code and Software is quite common but there is a subtle distinction
+                        between them. Software, in execution, is the end product while Code is what defines the Software
+                        and as such, Coding is the act that when performed, Software is created. Well crafted Code makes
+                        great Software but there isn’t such a thing as well crafted Software.
+                    </Typography>
+                    <Typography paragraph>
+                        My name is Juan Carlos Carrillo, best known as <b>JC Carrillo</b>: I’m an experienced Technology
+                        Leader, application architect and a coder at heart with over 20 years of success in
+                        planning, development, integration, monitoring and support of enterprise systems in e-commerce,
+                        human resources, logistics, social, finance and telecommunication industries.
+                    </Typography>
+                    <Typography paragraph>
+                        Throughout my career I have delivered multi-million dollar, highly scalable distributed
+                        enterprise systems some still in use and others extinct. Over the years I have learned and
+                        practiced all phases of the Software Development Life Cycle and this site is dedicated to
+                        sharing my findings and experiences as to help improve the coding profession.
+                    </Typography>
+                    <Typography paragraph>
+                        I belief in professionalism, mentorship and coding as craftsmanship and as a result of my
+                        beliefs I have put together the following manifesto – my public declaration of principles for
+                        the coding professional.
+                    </Typography>
+                    <Typography paragraph align={"center"}>
+                        <br/><b>Manifesto for Coding Craftsmanship</b><br/><br/>
+                        I am uncovering better ways to be a coding professional by doing it and helping others do
+                        it.<br/>
+                        Through this work, I have come to value: <br/><br/>
+
+                        Curiosity,<br/>
+                        Simplicity,<br/>
+                        Testing,<br/>
+                        Flexibility, and<br/>
+                        Collaboration<br/><br/>
+                        © 2016, JC Carrillo<br/>
+                        this declaration may be freely copied in any form,<br/>
+                        but only in its entirety through this notice.
+                    </Typography>
+                </Box>
+            </Box>
         </Box>
     );
 }
